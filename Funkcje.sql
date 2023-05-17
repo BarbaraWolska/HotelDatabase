81c4aa192d6c49153a1102dbb2fa70861f3508b0
Barbara Wolska <101739433+BarbaraWolska@users.noreply.github.com>
2023-05-17 23:44:25 +0200

    Update Funkcje.sql

--- Funkcje.sql
+++ Funkcje.sql
@@ -5,13 +5,7 @@
 use HotelDatabase
 go
 CREATE OR ALTER FUNCTION dbo.uf_IsPeselOK(
-@pesel varchar(11)
-)
-RETURNS tinyint
-AS
-BEGIN
-DECLARE @wagi AS TABLE(pozycja tinyint NOT NULL,
-                       waga tinyint NOT NULL)
+
 IF ISNUMERIC(@pesel) = 0 RETURN 0
 INSERT INTO @wagi(pozycja,waga)
 VALUES (1,1),(2,3),(3,7),(4,9),(5,1),
@@ -23,19 +17,7 @@ RETURN 1
 END
 GO
 
---walidacja numeru NIP
-use HotelDatabase
-go
-CREATE OR ALTER FUNCTION dbo.uf_IsNIPNumberOK(
-@NIPNumber varchar(10)
-)
-RETURNS tinyint
-AS
-BEGIN
-DECLARE @wagi AS TABLE(pozycja tinyint NOT NULL,
-                       waga tinyint NOT NULL)
-IF ISNUMERIC(@NIPNumber) = 0 RETURN 0
-INSERT INTO @wagi(pozycja,waga)
+
 VALUES (1,6),(2,5),(3,7),(4,2),(5,3),
        (6,4),(7,5),(8,6),(9,7)
 IF (SELECT SUM(CAST(SUBSTRING(@NIPNumber, pozycja, 1) AS tinyint) * waga) % 10
@@ -52,14 +34,8 @@ CREATE OR ALTER FUNCTION uf_SearchGuestByNameOrLastName(@NameOrSurname varchar(6
 RETURNS TABLE
 AS
 RETURN(
-SELECT g.IdGuest,g.IDNumber, g.LastName, g.Name, g.PassportNumber, pd.* 
-FROM [hotel].[tbl_guests] as g inner join [hotel].[tbl_people_data] as pd
-on g.IdPersonData=pd.IdPersonData
-WHERE LOWER([Name])+' '+LOWER([LastName]) LIKE LOWER('%'+@NameOrSurname+'%'))
-GO
 
---aktualny status podanego pokoju
-use HotelDatabase
+
 go
 CREATE OR ALTER FUNCTION uf_IsRoomFree(@id_room int)
 RETURNS bit
@@ -78,15 +54,7 @@ BEGIN
 RETURN @is_free
 END
 
---liczba rezerwacji dla goœcia o danym imieniu/nazwisku
-use HotelDatabase
-go
-CREATE OR ALTER FUNCTION uf_NumberOfGuestReservations
-(@NameOrLastName varchar(50))
-RETURNS TABLE
-AS
-RETURN(
-SELECT p.Name, p.LastName, p.IdGuest, COUNT(r.IdReservation) AS NumberOfReservation 
+ation) AS NumberOfReservation 
 FROM (SELECT g.Name, g.LastName, g.IdGuest, t.IdReservation
 FROM [hotel].[tbl_guests] as g, [hotel].[tbl_transactions] as t
 WHERE g.IdGuest = t.IdGuest) as p, [hotel].[tbl_reservations] as r
@@ -98,16 +66,7 @@ GO
 --wydobywanie ceny danego pokoju
 use HotelDatabase
 go
-CREATE OR ALTER FUNCTION uf_getRoomPrice(@id_room int)
-RETURNS money
-BEGIN
-DECLARE @price money
-	SET @price =  (SELECT [RoomPrice] FROM [room].[tbl_room_type] as rt
-	INNER JOIN [hotel].[tbl_rooms] as r
-	ON rt.[IdRoomType] = r.[IdRoomType]
-	WHERE r.[IdRoom] = @id_room)
-RETURN @price
-END
+
 GO
 
 --numer goœcia na podstawie numeru faktury
@@ -124,17 +83,7 @@ DECLARE @id int
 	WHERE IdBilling=@id_billing) 
 RETURN @id
 END
-GO
-
---data wymeldowania na podstawie numeru rezerwacji
-use HotelDatabase
-go
-CREATE OR ALTER FUNCTION uf_getCheckOutDate(@id_reservation int)
-RETURNS date
-BEGIN
-DECLARE @CheckOutDate date
-	SET @CheckOutDate = (SELECT CheckOutDate FROM [hotel].[tbl_reservations] WHERE [IdReservation] = @id_reservation)
-	RETURN @CheckOutDate
+=
 END
 GO
 
@@ -145,17 +94,7 @@ go
 CREATE FUNCTION uf_getCheckInDate(@id_reservation int)
 RETURNS date
 BEGIN
-DECLARE @CheckInDate date
-	SET @CheckInDate = (SELECT CheckInDate FROM [hotel].[tbl_reservations] WHERE [IdReservation] = @id_reservation)
-	RETURN @CheckInDate
-END
-GO
 
---obliczanie d³ugoœci pobytu w dniach
-use HotelDatabase
-go
-CREATE OR ALTER FUNCTION uf_LengthOfTheVisit(@id_Reservation int)
-Returns int
 BEGIN
 DECLARE @days int, @CheckInDate date, @CheckOutDate date
 	
@@ -175,13 +114,7 @@ use HotelDatabase
 go
 CREATE OR ALTER FUNCTION uf_setRoomStatusReserved(@id_room int)
 RETURNS bit
-BEGIN
-DECLARE @isRoomReserved bit, @CheckInDate date, @CheckOutDate date, @status varchar
-	Select @CheckInDate = [CheckInDate], @CheckOutDate = [CheckOutDate] 
-	from [hotel].[tbl_reservations]
-	where [IdRoom]=@id_room
 
-	select @status=[StatusName]
 	from [reservation].[tbl_reservation_status] as s
 	inner join [hotel].[tbl_reservations] as r
 	on s.[IdReservationStatus]=r.[IdReservationStatus]
@@ -199,11 +132,7 @@ GO
 use HotelDatabase
 go
 CREATE OR ALTER FUNCTION uf_SumOfBilling(@id_transaction int)
-RETURNS money
-BEGIN
-    DECLARE @SumOfBilling money = 0
-    
-    SELECT @SumOfBilling +=[dbo].[uf_LengthOfTheVisit](r.IdReservation) * [dbo].[uf_getRoomPrice](r.IdRoom)
+sit](r.IdReservation) * [dbo].[uf_getRoomPrice](r.IdRoom)
     FROM [hotel].[tbl_transactions] AS t
     JOIN [hotel].[tbl_reservations] AS r ON t.IdReservation = r.IdReservation
     WHERE t.IdTransaction = @id_transaction
